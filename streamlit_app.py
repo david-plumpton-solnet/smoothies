eb7ed9409cc6486726283952681088015cfa5a70
Solnet Solutions <david.plumpton@solnet.co.nz>
2024-06-12 14:53:37 +1200

    dbg

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,8 @@ if ingredients_list:
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        url = "https://fruityvice.com/api/fruit/" + search_on
+        st.write('url = "' + url + '"')
 
         st.subheader(fruit_chosen + ' Nutrition information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
